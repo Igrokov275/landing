37a9076ff4d0654f1f8669953d08931626c1756a
Igrokov <igrokov275@gmail.com>
2018-12-07 16:22:59 +0400

    Second

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+});
